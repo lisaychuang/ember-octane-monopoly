03d669e75fe0342c496d0e7b2595e7ee68157652
Lisa Huang <lisa.yc.huang@gmail.com>
2019-04-02 18:10:29 -0700

    Update helper to get nested obj values

--- app/helpers/get-name.js
+++ app/helpers/get-name.js
@@ -1,8 +1,16 @@
+import { get } from '@ember/object';
 import { helper } from '@ember/component/helper';
 import allNames from '../utils/names';
+
+
 export function getName(params/*, hash*/) {
   const key = params[0];
-  return allNames[key];
+  const path = params[1];
+  if (typeof path === 'undefined') {
+    return allNames[key];
+  } else {
+    return get(allNames[key], path);
+  }
 }
 
 export default helper(getName);
