335c320116a64712defc2c453d177847141fa846
Lisa Huang <lisa.yc.huang@gmail.com>
2019-04-04 17:22:11 -0700

    Change name mapping to board position mapping

--- app/utils/names.js
+++ app/utils/names.js
@@ -1,123 +1,292 @@
 export default {
-    chance: 'Chance',
-    communityChest: 'Community Chest', // Mars Cards
-    railroad1: 'Reading Railroad',
-    railroad2: 'Pennsylvania Railroad',
-    railroad3: 'B & O  Railroad',
-    railroad4: 'Short Line',
-    income: 'Income Tax',
-    luxury: 'Luxury Tax',
-    electric: 'Electric Company',
-    waterworks: 'Waterworks',
-    property1: {
+    0: {
+        name: 'Go'
+    },
+    1: {
         streetName: 'Med Avenue',
         price: 50,
         color: 'dark-purple'
     },
-    property2: {
+    2: {
+        name: 'Community Chest'
+    },
+    3: {
         streetName: 'Baltic Avenue',
         price: 50,
         color: 'dark-purple'
     },
-    property3: {
+    4: {
+        name: 'Income Tax'
+    },
+    5: {
+        name: 'Reading Railroad',
+        price: 200
+    },
+    6: {
         streetName: 'Oriental Avenue',
         price: 100,
         color: 'light-blue'
     },
-    property4: {
+    7: {
+        name: 'Chance'
+    },
+    8: {
         streetName: 'Vermont Avenue',
         price: 100,
         color: 'light-blue'
     },
-    property5: {
+    9: {
         streetName: 'Connecticut Avenue',
         price: 120,
         color: 'light-blue'
     },
-    property6: {
+    10: {
+        name: 'Jail'  // In jail / Just visiting
+    },
+    11: {
         streetName: 'St. Charles Place',
         price: 140,
         color: 'purple'
     },
-    property7: {
+    12: {
+        name: 'Electric Company',
+        price: 150
+    },
+    13: {
         streetName: 'States Avenue',
         price: 140,
         color: 'purple'
     },
-    property8: {
+    14: {
         streetName: 'Virginia Avenue',
         price: 160,
         color: 'purple'
     },
-    property9: {
+    15: {
+        name: 'Pennsylvania Railroad',
+        price: 200
+    },
+    16: {
         streetName: 'St. James Avenue',
         price: 180,
         color: 'orange'
     },
-    property10: {
+    17: {
+        name: 'Community Chest'
+    },
+    18: {
         streetName: 'Tennessee Avenue',
         price: 180,
         color: 'orange'
     },
-    property11: {
+    19: {
         streetName: 'New York Avenue',
         price: 200,
         color: 'orange'
     },
-    property12: {
+    20: {
+        name: 'Parking'
+    },
+    21: {
         streetName: 'Kentucky Avenue',
         price: 220,
         color: 'red'
     },
-    property13: {
+    22: {
+        name: 'Chance'
+    },
+    23: {
         streetName: 'Indiana Avenue',
         price: 220,
         color: 'red'
-    },
-    property14: {
+    },  
+    24: {
         streetName: 'Illinois Avenue',
         price: 240,
         color: 'red'
     },
-    property15: {
+    25: {
+        name: 'B & O  Railroad',
+        price: 200
+    },
+    26: {
         streetName: 'Atlantic Avenue',
         price: 260,
         color: 'yellow'
     },
-    property16: {
+    27: {
         streetName: 'Ventnor Avenue',
         price: 260,
         color: 'yellow'
     },
-    property17: {
+    28: {
+        name: 'Waterworks',
+        price: 120
+    },
+    29: {
         streetName: 'Marvin Gardens',
         price: 280,
         color: 'yellow'
     },
-    property18: {
+    30: {
+        name: 'Go To' //Go to Jail
+    },
+    31: {
         streetName: 'Pacific Avenue',
         price: 300,
         color: 'green'
     },
-    property19: {
+    32: {
         streetName: 'North C Avenue',
         price: 300,
         color: 'green'
     },
-    property20: {
+    33: {
+        name: 'Community Chest'
+    },
+    34: {
         streetName: 'Pennsylvania Avenue',
         price: 320,
         color: 'green'
     },
-    property21: {
+    35: {
+        name: 'Short Line',
+        price: 200
+    },
+    36: {
+        name: 'Chance'
+    },
+    37: {
         streetName: 'Park Place',
         price: 350,
         color: 'dark-blue'
     },
-    property22: {
+    38: {
+        name: 'Luxury Tax'
+    },
+    39: {
         streetName: 'Boardwalk',
         price: 400,
         color: 'dark-blue'
-    },
-
+    }
+    // chance: 'Chance',
+    // communityChest: 'Community Chest', // Mars Cards
+    // // railroad1: 'Reading Railroad',
+    // // railroad2: 'Pennsylvania Railroad',
+    // // railroad3: 'B & O  Railroad',
+    // // railroad4: 'Short Line',
+    // income: 'Income Tax',
+    // luxury: 'Luxury Tax',
+    // electric: 'Electric Company',
+    // waterworks: 'Waterworks',
+    // property1: {
+    //     streetName: 'Med Avenue',
+    //     price: 50,
+    //     color: 'dark-purple'
+    // },
+    // property2: {
+    //     streetName: 'Baltic Avenue',
+    //     price: 50,
+    //     color: 'dark-purple'
+    // },
+    // property3: {
+    //     streetName: 'Oriental Avenue',
+    //     price: 100,
+    //     color: 'light-blue'
+    // },
+    // property4: {
+    //     streetName: 'Vermont Avenue',
+    //     price: 100,
+    //     color: 'light-blue'
+    // },
+    // property5: {
+    //     streetName: 'Connecticut Avenue',
+    //     price: 120,
+    //     color: 'light-blue'
+    // },
+    // property6: {
+    //     streetName: 'St. Charles Place',
+    //     price: 140,
+    //     color: 'purple'
+    // },
+    // property7: {
+    //     streetName: 'States Avenue',
+    //     price: 140,
+    //     color: 'purple'
+    // },
+    // property8: {
+    //     streetName: 'Virginia Avenue',
+    //     price: 160,
+    //     color: 'purple'
+    // },
+    // property9: {
+    //     streetName: 'St. James Avenue',
+    //     price: 180,
+    //     color: 'orange'
+    // },
+    // property10: {
+    //     streetName: 'Tennessee Avenue',
+    //     price: 180,
+    //     color: 'orange'
+    // },
+    // property11: {
+    //     streetName: 'New York Avenue',
+    //     price: 200,
+    //     color: 'orange'
+    // },
+    // property12: {
+    //     streetName: 'Kentucky Avenue',
+    //     price: 220,
+    //     color: 'red'
+    // },
+    // property13: {
+    //     streetName: 'Indiana Avenue',
+    //     price: 220,
+    //     color: 'red'
+    // },
+    // property14: {
+    //     streetName: 'Illinois Avenue',
+    //     price: 240,
+    //     color: 'red'
+    // },
+    // property15: {
+    //     streetName: 'Atlantic Avenue',
+    //     price: 260,
+    //     color: 'yellow'
+    // },
+    // property16: {
+    //     streetName: 'Ventnor Avenue',
+    //     price: 260,
+    //     color: 'yellow'
+    // },
+    // property17: {
+    //     streetName: 'Marvin Gardens',
+    //     price: 280,
+    //     color: 'yellow'
+    // },
+    // property18: {
+    //     streetName: 'Pacific Avenue',
+    //     price: 300,
+    //     color: 'green'
+    // },
+    // property19: {
+    //     streetName: 'North C Avenue',
+    //     price: 300,
+    //     color: 'green'
+    // },
+    // property20: {
+    //     streetName: 'Pennsylvania Avenue',
+    //     price: 320,
+    //     color: 'green'
+    // },
+    // property21: {
+    //     streetName: 'Park Place',
+    //     price: 350,
+    //     color: 'dark-blue'
+    // },
+    // property22: {
+    //     streetName: 'Boardwalk',
+    //     price: 400,
+    //     color: 'dark-blue'
+    // },
 }
