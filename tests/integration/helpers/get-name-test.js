7c547200a1794e600024c0eb2d6f6d01709f99b2
Lisa Huang <lisa.yc.huang@gmail.com>
2019-04-04 17:29:41 -0700

    Update helper to get-value

--- tests/integration/helpers/get-name-test.js
+++ tests/integration/helpers/get-name-test.js
@@ -10,7 +10,7 @@ module('Integration | Helper | get-name', function(hooks) {
   test('it renders', async function(assert) {
     this.set('inputValue', '1234');
 
-    await render(hbs`{{get-name inputValue}}`);
+    await render(hbs`{{get-value inputValue}}`);
 
     assert.equal(this.element.textContent.trim(), '1234');
   });
