a57a6d607423e8c20ba930a3798d286cf1b10544
Lisa Huang <lisa.yc.huang@gmail.com>
2019-04-08 16:09:46 -0700

    Create onDiceRoll method in gameboard component

--- app/components/gameboard.js
+++ app/components/gameboard.js
@@ -1,6 +1,15 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 export default class GameboardComponent extends Component {
 
-    
+    @action
+    onDiceRoll(){
+        const { game } = this.args;
+        if (game.currentPlayer.isInJail) {
+            game.getOutOfJail();
+        } else {
+            game.rollForCurrentPlayer();
+        }
+    }
 }
