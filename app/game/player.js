fa23e8444f4790b5fcd3fd3dd8a0e86da430068f
Lisa Huang <lisa.yc.huang@gmail.com>
2019-04-08 14:26:00 -0700

    Create tripleDoubleRolls method for player class

--- app/game/player.js
+++ app/game/player.js
@@ -1,6 +1,6 @@
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { getValue } from '../helpers/get-value';
+// import { getValue } from '../helpers/get-value';
 
 export default class Player {
     @tracked
@@ -10,7 +10,13 @@ export default class Player {
     positionOnBoard = 0
 
     @tracked
-    properties = []; 
+    titleDeeds = []; 
+
+    @tracked
+    doublesCount = 0;
+
+    @tracked
+    isInJail = false;
 
     constructor(name, token = '🐹') {
         this.name = name;
@@ -22,9 +28,22 @@ export default class Player {
         this.positionOnBoard += diceTotal;
     }
 
+    // IF player throws double 3x, go to jail: 
+    tripleDoubleRolls() {
+        this.positionOnBoard = 10;
+        this.isInJail = true;
+        this.doublesCount = 0;
+    }
+
+
+    // Player should update titleDeeds after each purchase
+
     // buyProperty(boardPosition) {
     //     this.money -= getValue([boardPosition, 'price']);
     //     this.properties.push()
     // }
+
+    // Player's positionOnBoard resets after passing 38 (arrives at Go, which is position 0)
+        // also receives $200 at this time
 }
 
